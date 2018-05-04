1cfc3fb10523c79e403c7260f1c1d12b3efcd879
hiroshi <hiroshi3110@gmail.com>
2018-05-04 19:29:24 +0900

    Start fdb-search server.

--- main.go
+++ main.go
@@ -7,43 +7,34 @@ import (
   "github.com/apple/foundationdb/bindings/go/src/fdb/tuple"
 	"log"
 	"fmt"
+	"net/http"
 	"strings"
 	"unicode/utf8"
 )
 
-// func createIndex(text string) {
-	
-// }
-
-func main() {
-	// Different API versions may expose different runtime behaviors.
-	fdb.MustAPIVersion(510)
+func createIndex(partition string, id string, inputText string) {
 	// Open the default database from the system cluster
 	db := fdb.MustOpenDefault()
-
-
 	// directory
 	dir, err := directory.CreateOrOpen(db, []string{"gyazo"}, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	// subspace
-	termSS := dir.Sub("term")
-
+	ss := dir.Sub(partition)
 
 	// Create index
-	fmt.Printf("Create Indexes\n")
+	// fmt.Printf("Create Indexes\n")
 	_, err = db.Transact(func (tr fdb.Transaction) (ret interface{}, e error) {
-		text := strings.ToLower("Windows と macOS")
-		fmt.Printf("  text: %v\n", text)
-		// tr.Set(fdb.Key("hello"), []byte{})
-		// fmt.Printf("hello: %v\n", tr.Get(fdb.Key("hello")).MustGet())
+		// text := strings.ToLower("Windows と macOS")
+		// fmt.Printf("  text: %v\n", text)
+		text := strings.ToLower(inputText)
 
 		for i, w := 0, 0; i < len(text); i+= w {
 			runeValue, width := utf8.DecodeRuneInString(text[i:])
 			// key := termSS.Pack(tuple.Tuple{string(runeValue), i})
-			key := termSS.Sub(string(runeValue)).Pack(tuple.Tuple{i})
-			fmt.Printf("%v\n", key)
+			// key := ss.Sub(string(runeValue)).Pack(tuple.Tuple{i})
+			key := ss.Sub(string(runeValue), id, i)
+			// fmt.Printf("%v\n", key)
 			tr.Set(key, []byte("\x01"))
 			w = width
 		}
@@ -52,16 +43,29 @@ func main() {
 	if err != nil {
 		log.Fatalf("Unable to set FDB database value (%v)", err)
 	}
+}
 
-	// Search index
-	fmt.Printf("Search Indexes\n")
+// type SearchResult struct {
+// 	: 
+// }
+
+func search(partition string, term string) []string {
+	// Open the default database from the system cluster
+	db := fdb.MustOpenDefault()
+	// directory
+	dir, err := directory.CreateOrOpen(db, []string{"gyazo"}, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ss := dir.Sub(partition)
+
 	_, err = db.Transact(func (tr fdb.Transaction) (ret interface{}, e error) {
-		text := strings.ToLower("mac")
-		fmt.Printf("  text: %v\n", text)
+		text := strings.ToLower(term)
+		// fmt.Printf("  text: %v\n", text)
 
 		runes := []rune(text)
 
-		key := termSS.Sub(string(runes[0]))
+		key := ss.Sub(string(runes[0]))
 		ri := tr.GetRange(key, fdb.RangeOptions{}).Iterator()
 		for ri.Advance() {
 			// First rune
@@ -77,9 +81,7 @@ func main() {
 			// next runes
 			for i := 1; i < len(runes); i++ {
 				fmt.Printf("i: %v, rune: %v\n", i, string(runes[i]))
-				nextKey := termSS.Sub(string(runes[i])).Pack(tuple.Tuple{int(pos) + i})
-				// ret := tr.Get(nextKey).MustGet()
-				// fmt.Printf("ret: %v\n", ret)
+				nextKey := ss.Sub(string(runes[i])).Pack(tuple.Tuple{int(pos) + i})
 				fmt.Printf("key: %v\n", nextKey)
 				v := tr.Get(nextKey).MustGet()
 				fmt.Printf("v: %v\n", v)
@@ -88,69 +90,87 @@ func main() {
 					match = false
 					break
 				}
-				// fmt.Printf("v: %v\n", kv.)
-				// if err != nil {
-				// 	fmt.Printf("not matched\n")
-				// 	match = false
-				// 	break
-				// }
 			}
 			if match {
 				fmt.Printf("matched position: %v\n", pos)
 			}
-			// fmt.Printf("%v\n", t)
 		}
-
-		// for i, w := 0, 0; i < len(text); i+= w {
-		// 	runeValue, width := utf8.DecodeRuneInString(text[i:])
-		// 	key := termSS.Sub(string(runeValue))
-		// 	ri := tr.GetRange(key, fdb.RangeOptions{}).Iterator()
-		// 	for ri.Advance() {
-		// 		kv := ri.MustGet()
-		// 		// fmt.Printf("%v\n", kv)
-		// 		t, err := key.Unpack(kv.Key)
-		// 		if err != nil {
-		// 			log.Fatalf("Uppack failed")
-		// 		}
-		// 		// fmt.Printf("%v\n", t)
-		// 	}
-		// 	// fmt.Printf("%v\n", rangeResults)
-		// 	w = width
-		// }
 		return
 	})
 	if err != nil {
 	    log.Fatalf("Unable to read FDB database value (%v)", err)
 	}
+	return []string{}
+}
+
+func getParamOrErrorResponse(w http.ResponseWriter, params map[string][]string, name string) string {
+	if len(params[name]) == 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Error: No '%s' param specified.\n", name)
+		return ""
+	}
+	return params[name][0]
+}
 
 
+func postIndexHandler(w http.ResponseWriter, r *http.Request) {
+	if (r.Method != "POST") {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintf(w, "Error: POST only.\n")
+		return
+	}
+	// r.ParseForm()
+	r.ParseMultipartForm(1024*1024)
+	log.Printf("POST /index form:%v\n", r.Form)
 
+	partition := getParamOrErrorResponse(w, r.Form, "partition")
+	if partition == "" {
+		return
+	}
+	id := getParamOrErrorResponse(w, r.Form, "id")
+	if id == "" {
+		return
+	}
+	content := getParamOrErrorResponse(w, r.Form, "content")
+	if content == "" {
+		return
+	}
 
+	createIndex(partition, id, content)
+	fmt.Fprintf(w, "Index created for partition='%s', id='%s'\n", partition, id)
+}
 
-	// courseSS := schedulingDir.Sub("class")
-	// fmt.Printf("courseSS: %v\n", courseSS)
-	// attendSS := schedulingDir.Sub("attends")
-	// fmt.Printf("attendSS: %v\n", attendSS)
+func getSearchHandler(w http.ResponseWriter, r *http.Request) {
+	if (r.Method != "GET") {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintf(w, "Error: GET only.\n")
+		return
+	}
+	q := r.URL.Query()
+	log.Printf("GET /search query:%v\n", q)
+	partition := getParamOrErrorResponse(w, q, "partition")
+	if partition == "" {
+		return
+	}
+	term := getParamOrErrorResponse(w, q, "term")
+	if term == "" {
+		return
+	}
 
-	// _, err = db.Transact(func (tr fdb.Transaction) (ret interface{}, e error) {
-	// 	// tr.Set(fdb.Key("hello2"), []byte("world"))
-	// 	key := attendSS.Pack(tuple.Tuple{"hiroshi", "math"})
-	// 	fmt.Printf("%v\n", key)
-	// 	tr.Set(key, []byte{})
-	// 	return
-	// })
-	// if err != nil {
-	// 	log.Fatalf("Unable to set FDB database value (%v)", err)
-	// }
+	ids := search(partition, term)
+	fmt.Fprintf(w, "Found: %v\n", ids)
+}
 
-	// ret, err := db.Transact(func (tr fdb.Transaction) (ret interface{}, e error) {
-	//     ret = tr.Get(fdb.Key("hello")).MustGet()
-	//     return
-	// })
-	// if err != nil {
-	//     log.Fatalf("Unable to read FDB database value (%v)", err)
-	// }
+func main() {
+	// Different API versions may expose different runtime behaviors.
+	fdb.MustAPIVersion(510)
 
-	// v := ret.([]byte)
-	// fmt.Printf("hello, %s\n", string(v))
+	// createIndex("Windows と macOS")
+	// search("mac")
+
+	http.HandleFunc("/index", postIndexHandler)
+	http.HandleFunc("/search", getSearchHandler)
+	addr := ":12345"
+	log.Printf("Starging server: %s\n", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
 }
